3f1e87d22e7ce0939e42514b553a864e97faf2a9
eimamovic2 <eimamovic2@etf.unsa.ba>
2023-01-13 11:04:51 +0100

    fixes and video detail part

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -16,7 +16,7 @@ const Feed = () => {
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
-      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx:0, md: 2}}}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2}}}>
         <Sidebar
           selectedCategory={selectedCategory}
           setSelectedCategory={setSelectedCategory}
@@ -50,7 +50,7 @@ const Feed = () => {
         <Videos videos={videos} />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
